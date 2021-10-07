5054f2b26218ad22752ecf046e63742a67fb65ab
M Shashanka <shashanka318@gmail.com>
2021-10-07 21:06:17 +0530

    setting up the server

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const server = require('http').createServer(app);
+
+const next = require('next');
+const dev = process.env.NODE_ENV !== 'production';
+const nextApp = next({ dev });
+const handle = nextApp.getRequestHandler();
+
+app.use(express.json());
+
+require('dotenv').config({path: "./config.env"});
+const PORT = process.env.PORT || 3000;
+
+const connectDb = require('./utilsServer/connectDb');
+connectDb();
+
+nextApp.prepare().then(() => {
+//(files/pages) in nextJS are server side rendered, so without below line pages folder files won't work
+    app.all('*', (req, res) => handle(req, res)); //allow nextJS to handle SSR
+
+    server.listen(PORT, err => {
+        if(err) throw err;
+        console.log(`Express server running on ${PORT}`);
+    });
+});
