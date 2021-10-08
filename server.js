c9e57d1fbd46fd5f6bd546d9361629e4a6cff4f1
M Shashanka <shashanka318@gmail.com>
2021-10-09 04:40:22 +0530

    prepare backend for signup & login

--- server.js
+++ server.js
@@ -1,26 +1,29 @@
+require("dotenv").config({ path: "./config.env" });
 const express = require("express");
 const app = express();
-const server = require('http').createServer(app);
+const server = require("http").createServer(app);
 
-const next = require('next');
-const dev = process.env.NODE_ENV !== 'production';
+const next = require("next");
+const dev = process.env.NODE_ENV !== "production";
 const nextApp = next({ dev });
 const handle = nextApp.getRequestHandler();
 
 app.use(express.json());
 
-require('dotenv').config({path: "./config.env"});
 const PORT = process.env.PORT || 3000;
 
-const connectDb = require('./utilsServer/connectDb');
+const connectDb = require("./utilsServer/connectDb");
 // connectDb();
 
 nextApp.prepare().then(() => {
-//(files/pages) in nextJS are server side rendered, so without below line pages folder files won't work
-    app.all('*', (req, res) => handle(req, res)); //allow nextJS to handle SSR
+  app.use("/api/signup", require("./api/signup"));
+  app.use("/api/auth", require("./api/auth"));
 
-    server.listen(PORT, err => {
-        if(err) throw err;
-        console.log(`Express server running on ${PORT}`);
-    });
+  //(files/pages) in nextJS are server side rendered, so without below line pages folder files won't work
+  app.all("*", (req, res) => handle(req, res)); //allow nextJS to handle SSR
+
+  server.listen(PORT, (err) => {
+    if (err) throw err;
+    console.log(`Express server running on ${PORT}`);
+  });
 });
